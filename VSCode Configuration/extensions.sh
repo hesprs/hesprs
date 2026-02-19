05801a4aca23962c4b9ad3f4850cac770dad147d
Hēsperus <hesprs@outlook.com>
2026-02-19 23:45:30 +0800

    routine

--- VSCode Configuration/extensions.sh
+++ VSCode Configuration/extensions.sh
@@ -1,19 +1,22 @@
 # codium --list-extensions | xargs -L 1 echo codium --install-extension
 
+codium --install-extension alibaba-cloud.tongyi-lingma
+codium --install-extension bierner.emojisense
 codium --install-extension bierner.markdown-mermaid
 codium --install-extension catppuccin.catppuccin-vsc-icons
+codium --install-extension charkoal.charkoal
 codium --install-extension charliermarsh.ruff
-codium --install-extension codeium.codeium
+codium --install-extension expo.vscode-expo-theme
+codium --install-extension foam.foam-vscode
 codium --install-extension jnoortheen.nix-ide
 codium --install-extension miguelsolorio.fluent-icons
 codium --install-extension ms-python.python
 codium --install-extension ms-toolsai.jupyter
 codium --install-extension ms-toolsai.jupyter-renderers
 codium --install-extension oxc.oxc-vscode
+codium --install-extension pomdtr.excalidraw-editor
 codium --install-extension redhat.java
-codium --install-extension ritwickdey.liveserver
 codium --install-extension rust-lang.rust-analyzer
 codium --install-extension sguerri.simple-hide-files
-codium --install-extension sst-dev.opencode
 codium --install-extension streetsidesoftware.code-spell-checker
 codium --install-extension vue.volar
